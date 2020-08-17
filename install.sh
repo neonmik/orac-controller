8c4bce6c694d42293a7bd12fe9321367b7e3774d
neonmik <63063633+neonmik@users.noreply.github.com>
2020-08-17 13:30:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ BASE_PATH=$(dirname $(readlink -f $0))
 
 apt install python3-pip libjack-jackd2-dev
 pip3 install python-rtmidi python-osc
-install -v -m 644 $BASE_PATH/orac-bridge/99-orac-ctl-bridge.rules /etc/udev/rules.d/
-install -v -m 644 $BASE_PATH/orac-bridge/orac-ctl-bridge.service /usr/lib/systemd/system/
-install -v -m 755 $BASE_PATH/orac-bridge/OracCtlBridge.py /usr/local/bin/
+install -v -m 644 $BASE_PATH/orac-bridge/99-orac-bonnet-bridge.rules /etc/udev/rules.d/
+install -v -m 644 $BASE_PATH/orac-bridge/orac-bonnet-bridge.service /usr/lib/systemd/system/
+install -v -m 755 $BASE_PATH/orac-bridge/OracBonnetBridge.py /usr/local/bin/
 systemctl daemon-reload
 udevadm control --reload
